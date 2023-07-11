5b8bec62a07ec4e8eecef8af75d158f72ae96578
Shalqarov <shalqarov01@gmail.com>
2023-07-11 17:55:52 +0600

    update

--- ckalkan/dl_open.py
+++ ckalkan/dl_open.py
@@ -19,7 +19,9 @@ class LibHandle:
         self.handle.KC_GetFunctionList()
         return self.handle.Init()
 
-    def load_key_store(self, path: str, password: str, store_type=1, alias: str = "") -> int:
+    def load_key_store(
+            self, path: str, password: str, store_type=1, alias: str = ""
+    ) -> int:
         """
         Загрузка ключей/сертификата их хранилища.
         :param path: название/путь хранилища
@@ -33,8 +35,12 @@ class LibHandle:
         c_container = ctypes.c_char_p(path.encode())
         c_alias = ctypes.c_char_p(alias.encode())
         return self.handle.KC_LoadKeyStore(
-            ctypes.c_int(store_type), c_password, ctypes.c_int(len(password)),
-            c_container, ctypes.c_int(len(path)), c_alias
+            ctypes.c_int(store_type),
+            c_password,
+            ctypes.c_int(len(password)),
+            c_container,
+            ctypes.c_int(len(path)),
+            c_alias,
         )
 
 

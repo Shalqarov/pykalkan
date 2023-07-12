76552dbe12e6bd454374af5867bff522e7cfaed1
Shalqarov <shalqarov01@gmail.com>
2023-07-12 17:58:36 +0600

    add: new errors

--- kalkan_crypt/ckalkan/error_codes.py
+++ kalkan_crypt/ckalkan/error_codes.py
@@ -15,12 +15,23 @@ class ErrorCode:
         return f"Неизвестный код ошибки: {self.code}"
 
 
-ERROR_CODE_OK: Final[ErrorCode] = ErrorCode(0)  # Нет ошибки
-ERROR_CODE_ERROR_READ_PKCS12: Final[ErrorCode] = ErrorCode(149946370)  # Невозможно прочитать файл формата pkcs#12
-ERROR_CODE_INVALID_PASSWORD: Final[ErrorCode] = ErrorCode(149946377)  # Неправильный пароль
+KC_OK: Final[ErrorCode] = ErrorCode(0)  # Нет ошибки
+KC_ERROR_READ_PKCS12: Final[ErrorCode] = ErrorCode(149946370)  # Невозможно прочитать файл формата pkcs#12
+KC_ERROR_OPEN_PKCS12: Final[ErrorCode] = ErrorCode(149946371)  # Невозможно открыть файл формата pkcs#12
+KC_INVALID_PROP_ID: Final[ErrorCode] = ErrorCode(149946372)  # Недопустимый идентификатор расширения сертификата
+KC_BUFFER_TOO_SMALL: Final[ErrorCode] = ErrorCode(149946373)  # Размер буфера слишком мал
+KC_CODE_CERT_PARSE_ERROR: Final[ErrorCode] = ErrorCode(149946374)  # Невозможно разобрать (распарсить) сертификат
+KC_INVALID_FLAG: Final[ErrorCode] = ErrorCode(149946375)  # Недопустимый флаг
+KC_OPEN_FILE_ERR: Final[ErrorCode] = ErrorCode(149946376)  # Невозможно открыть файл
+KC_INVALID_PASSWORD: Final[ErrorCode] = ErrorCode(149946377)  # Неправильный пароль
+KC_MEMORY_ERROR: Final[ErrorCode] = ErrorCode(149946381)  # Невозможно выделить память
+KC_CHECK_CHAIN_ERROR: Final[ErrorCode] = ErrorCode(
+    149946382)  # Не найден сертификат УЦ или сертификат пользователя при проверки цепочки
+KC_VALID_TYPE_ERROR: Final[ErrorCode] = ErrorCode(149946384)  # Недопустимый тип валидации сертификата
+KC_BAD_CRL_FORMAT: Final[ErrorCode] = ErrorCode(149946385)  # Некорректный формат CRL
 
 error_labels = {
-    ERROR_CODE_OK.code: "Нет ошибки",
-    ERROR_CODE_ERROR_READ_PKCS12.code: "Невозможно прочитать файл формата pkcs#12",
-    ERROR_CODE_INVALID_PASSWORD.code: "Неправильный пароль",
+    KC_OK.code: "Нет ошибки",
+    KC_ERROR_READ_PKCS12.code: "Невозможно прочитать файл формата pkcs#12",
+    KC_INVALID_PASSWORD.code: "Неправильный пароль",
 }

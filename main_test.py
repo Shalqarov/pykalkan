4efd9cbcbfd24de535a85c0eb7302d16dfe93978
Shalqarov <shalqarov01@gmail.com>
2023-08-19 14:40:10 +0600

    update: test

--- main_test.py
+++ main_test.py
@@ -1,78 +1,49 @@
 import os
 import sys
+import time
 
 import pytest
 from dotenv import load_dotenv
 
-from kalkan_crypt.C.error_codes import wrap_error
+from kalkan_crypt.C.error_codes import KalkanException, ValidateException, ErrorCode
 from kalkan_crypt.adapter import Adapter
 
 load_dotenv()
 
 library = "libkalkancryptwr-64.so.2.0.2"
 
-cert_path = "gost.p12"
-cert_password = "Qwerty12"  # Test
-
 
 @pytest.mark.parametrize(
-    "sign,data",
+    "path,password",
     [
-        (os.getenv("TEST_SIGN_BIN"), os.getenv("TEST_DATA_BIN")),
-        (os.getenv("TEST_SIGN_IIN"), os.getenv("TEST_DATA_IIN")),
+        (os.getenv("CERT_PATH"), os.getenv("CERT_PASSWORD")),
     ],
 )
-def test(sign, data):
+def test_main(path, password):
     cli = Adapter(library)
-    status = cli.init()
-    if status != 0:
-        assert False, f"Lib Init failed: {wrap_error(status)}"
-
-    status, res = cli.verify_data(sign, data)
-    if status != 0:
-        assert False, f"Verify Data failed: {wrap_error(status)}"
-    if "Verify - FAILED" in res["Info"].decode("utf-8"):
-        assert False, f"Verify Data failed: {res['Info']}"
-
-    status, res = cli.x509_certificate_get_info(res["Cert"].decode("utf-8"))
-    if sign == os.getenv("TEST_SIGN_BIN"):
-        if status != 0:
-            assert False, f"Get Info failed: {wrap_error(status)}"
-        if not res.decode("utf-8").startswith("OU=BIN"):
-            assert (
-                False
-            ), f"Certificate get info failed, must start with OU=BIN...: {res}"
-    else:
-        if status == 0:
-            assert False, f"get info must fail with IIN: {wrap_error(status)}"
-
-    cli.finalize()
-
-
-def main():
-    cli = Adapter(library)
-    status = cli.init()
-    if status != 0:
-        print(f"Lib Init failed: {wrap_error(status)}")
+    try:
+        cli.init()
+        cli.set_tsa_url()
+        cli.load_key_store(path, password)
+        data = cli.sign_data("SGVsbG8sIFdvcmxkIQ==")
+        if not is_valid_date(cli.get_time_from_sign(data.decode())):
+            raise KalkanException(ErrorCode.InvalidTime, "GET TIME")
+        res = cli.verify_data(data.decode("utf-8"), "SGVsbG8sIFdvcmxkIQ==")
+        _ = cli.x509_validate_certificate(res["Cert"].decode())
+    except ValidateException as ve:
+        print(f"Validate failed: {ve}")
         sys.exit(1)
-
-    print(cli.load_key_store(os.getenv("CERT_PATH"), os.getenv("CERT_PASSWORD")))
-
-    _, data = cli.sign_data("dGVzdA==")
-
-    status, res = cli.verify_data(data.decode("utf-8"), os.getenv("TEST_DATA_IIN"))
-    if status != 0:
-        print(f"Verify Data failed: {wrap_error(status)}")
+    except KalkanException as ke:
+        print(f"Kalkan Exception failed: {ke}")
         sys.exit(1)
-    if "Verify - FAILED" in res["Info"].decode("utf-8"):
-        print(f"Verify Data failed: {wrap_error(status)}")
-        sys.exit(1)
-
-    print(res["Info"].decode("utf-8"))
-    status, validated = cli.x509_validate_certificate(res["Cert"].decode("utf-8"))
-    print(f"{status} , response: {validated['response']},   info: {validated['info']}")
-    cli.finalize()
+    finally:
+        cli.finalize()
 
 
-if __name__ == "__main__":
-    main()
+def is_valid_date(timestamp):
+    try:
+        timestamp = int(timestamp)
+        time.gmtime(timestamp)
+        return True
+    except ValueError:
+        return False

68b999d604088ac389dd68a75a5eea5807fc5f12
Shalqarov <shalqarov01@gmail.com>
2023-08-21 12:50:44 +0600

    Added global variables in __init__ file for easier access
    In the __init__.py file within the kalkan_crypt module, global variables including SignatureFlag, StorageType, CertProp, CertCode, ValidateType from the enums module, ErrorCode, KalkanException, ValidateException from the error_codes module, and the Adapter from the adapter module were imported. This was done to provide easier access to these variables and classes throughout the entire module.

---
+++ kalkan_crypt/__init__.py
@@ -0,0 +1,3 @@
+from .C.enums import SignatureFlag, StorageType, CertProp, CertCode, ValidateType
+from .C.error_codes import ErrorCode, KalkanException, ValidateException
+from .adapter import Adapter

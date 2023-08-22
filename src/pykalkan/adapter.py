685e3ac9f8601e1149115b37c9b057c848be5f79
Shalqarov <shalqarov01@gmail.com>
2023-08-22 17:55:42 +0600

    Implement Singleton pattern and add docstrings in adapter.py
    Updated the Adapter class in adapter.py to use the singleton pattern, ensuring that only one instance of this class is created. This prevents multiple instances of this class from being created inadvertently, thus, maintaining a single state throughout the application's lifecycle.
    Also, detailed docstrings were added to all public methods for better understanding of their functionalities. This increases maintainability and readability, providing clear explanations about what each method does, what are the input parameters and what they return.

--- src/pykalkan/adapter.py
+++ src/pykalkan/adapter.py
@@ -3,41 +3,101 @@ from .interface import KalkanInterface
 
 
 class Adapter(KalkanInterface):
-    def __init__(self, lib: str):
-        self._kc = _KCCLient(lib)
+    """
+    Adapter
+    Класс, представляющий адаптер для криптографической библиотеки Kalkan.
+    """
+    _instance = None
+
+    def __new__(cls, lib: str):
+        if cls._instance is None:
+            cls._instance = super(Adapter, cls).__new__(cls)
+            cls._instance._kc = _KCCLient(lib)
+            cls._instance.init()
+        return cls._instance
 
     def init(self):
+        """Инициализация библиотеки.."""
         self._kc.kc_init()
 
     def load_key_store(self, cert_path: str, cert_password: str):
+        """
+        Загружает хранилище ключей из заданного пути к файлу сертификата и пароля.
+        :param cert_path: str - Путь к файлу сертификата.
+        :param cert_password: str - Пароль для сертификата.
+        """
         self._kc.load_key_store(cert_path, cert_password)
 
     def finalize(self):
+        """Освобождает ресурсы криптопровайдера KalkanCryptCOM и завершает работу библиотеки."""
         self._kc.finalize()
 
     def x509_export_certificate_from_store(self) -> bytes:
+        """
+        Экспорт сертификата из хранилища.
+        :return: bytes - Экспортированный сертификат.
+        """
         return self._kc.x509_export_certificate_from_store()
 
     def x509_load_certificate_from_buffer(self, in_cert: str):
+        """
+        Загрузка сертификата из памяти.
+        :param in_cert: str - сертификат.
+        """
         self._kc.x509_load_certificate_from_buffer(in_cert)
 
     def x509_certificate_get_info(self, in_cert: str) -> bytes:
+        """
+        Обеспечивает получение значений полей/расширений из сертификата.
+        :param in_cert: str - Сертификат, для которого необходимо получить информацию.
+        :return: bytes - Информация о сертификате.
+        """
         return self._kc.x509_certificate_get_info(in_cert)
 
     def sign_data(self, data) -> bytes:
+        """
+        Подписывает данные.
+        :param data - Данные для подписи.
+        :return: bytes - Подписанные данные.
+        """
         return self._kc.sign_data(data)
 
     def verify_data(self, signature: str, data: str) -> dict[str, bytes]:
+        """
+        Обеспечивает проверку подписи.
+        :param signature: str - Проверяемая подпись.
+        :param data: str - Проверяемые данные.
+        :return: dict[str, bytes] - Словарь, содержащий результат проверки.
+        """
         return self._kc.verify_data(signature, data)
 
     def x509_validate_certificate_ocsp(self, in_cert: str) -> dict[str, bytes]:
+        """
+        Проверка заданного сертификата на валидность с помощью OCSP.
+        :param in_cert: str - Сертификат для проверки.
+        :return: dict[str, bytes] - Словарь, содержащий результат проверки.
+        """
         return self._kc.x509_validate_certificate_ocsp(in_cert)
 
     def x509_validate_certificate_crl(self, in_cert: str, crl_path: str) -> dict[str, bytes]:
+        """
+        Проверка заданного сертификата на валидность с помощью CRL.
+        :param in_cert: str - Сертификат для проверки.
+        :param crl_path: str - Путь к файлу CRL.
+        :return: dict[str, bytes] - Словарь, содержащий результат проверки.
+        """
         return self._kc.x509_validate_certificate_crl(in_cert, crl_path)
 
     def get_time_from_sign(self, sign: str) -> int:
+        """
+        Извлекает временную метку из подписи.
+        :param sign: str - подпись, из которой нужно извлечь временную метку.
+        :return: int - временная метка.
+        """
         return self._kc.get_time(sign)
 
     def set_tsa_url(self):
+        """
+        Установка адреса сервиса TSA.
+        """
         self._kc.set_tsa_url()

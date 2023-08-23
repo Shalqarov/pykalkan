0fb2c8fe6c479920a264b5654c644748d5108346
Temirlan Shalqarov <93934831+Shalqarov@users.noreply.github.com>
2023-08-23 10:31:36 +0600

    threading lock (#10)
    * Implement Singleton pattern and add docstrings in adapter.py
    
    Updated the Adapter class in adapter.py to use the singleton pattern, ensuring that only one instance of this class is created. This prevents multiple instances of this class from being created inadvertently, thus, maintaining a single state throughout the application's lifecycle.
    Also, detailed docstrings were added to all public methods for better understanding of their functionalities. This increases maintainability and readability, providing clear explanations about what each method does, what are the input parameters and what they return.
    
    * Add thread-safety to Adapter class and implement Singleton pattern
    
    Added a lock to the Adapter class in adapter.py to ensure thread-safety and consistent state of the class instance. With threading.Lock(), multiple threads can safely instantiate and interact with the Adapter class without causing race conditions.
    Also, updated Adapter class to use Singleton design pattern to ensure that only one instance exists within the application context. This prevents the accidental creation of multiple
    
    * Update pykalkan project version
    
    The version in pyproject.toml was updated from 0.6.1 to 0.6.4. This update corresponds to significant changes in the structure and functionality of the project. Specifically, thread-safety has been added to the Adapter class and the Singleton design pattern has been implemented. This ensures multiple threads can interact safely with the Adapter class and only one instance of the Adapter class exists within the application, preventing accidental creation of new instances and maintaining a consistent state.
    
    * Update pykalkan to version 0.7.0
    
    The project version in pyproject.toml was updated from 0.6.4 to 0.7.0. This update reflects major improvements in the project like the addition of thread-safety to the Adapter class and implementation of Singleton design pattern. These changes enhance consistency and safer interactions within the project.
    
    * update lock var now inits in __new__func
    
    * Update library name in main_test.py
    
    * Refactor hardcoded data to variable in main_test.py

--- src/pykalkan/adapter.py
+++ src/pykalkan/adapter.py
@@ -1,43 +1,118 @@
+import threading
+
 from .C.kalkan import KCCLient as _KCCLient
 from .interface import KalkanInterface
 
 
 class Adapter(KalkanInterface):
-    def __init__(self, lib: str):
-        self._kc = _KCCLient(lib)
+    """
+    Adapter
+    Класс, представляющий адаптер для криптографической библиотеки Kalkan.
+    """
+    _instance = None
+
+    def __new__(cls, lib: str):
+        if cls._instance is None:
+            cls._instance = super(Adapter, cls).__new__(cls)
+            cls._lock = threading.Lock()
+            cls._instance._kc = _KCCLient(lib)
+            cls._instance.init()
+        return cls._instance
 
     def init(self):
-        self._kc.kc_init()
+        """Инициализация библиотеки.."""
+        with self._lock:
+            self._kc.kc_init()
 
     def load_key_store(self, cert_path: str, cert_password: str):
-        self._kc.load_key_store(cert_path, cert_password)
+        """
+        Загружает хранилище ключей из заданного пути к файлу сертификата и пароля.
+        :param cert_path: str - Путь к файлу сертификата.
+        :param cert_password: str - Пароль для сертификата.
+        """
+        with self._lock:
+            self._kc.load_key_store(cert_path, cert_password)
 
     def finalize(self):
-        self._kc.finalize()
+        """Освобождает ресурсы криптопровайдера KalkanCryptCOM и завершает работу библиотеки."""
+        with self._lock:
+            self._kc.finalize()
 
     def x509_export_certificate_from_store(self) -> bytes:
-        return self._kc.x509_export_certificate_from_store()
+        """
+        Экспорт сертификата из хранилища.
+        :return: bytes - Экспортированный сертификат.
+        """
+        with self._lock:
+            return self._kc.x509_export_certificate_from_store()
 
     def x509_load_certificate_from_buffer(self, in_cert: str):
-        self._kc.x509_load_certificate_from_buffer(in_cert)
+        """
+        Загрузка сертификата из памяти.
+        :param in_cert: str - сертификат.
+        """
+        with self._lock:
+            self._kc.x509_load_certificate_from_buffer(in_cert)
 
     def x509_certificate_get_info(self, in_cert: str) -> bytes:
-        return self._kc.x509_certificate_get_info(in_cert)
+        """
+        Обеспечивает получение значений полей/расширений из сертификата.
+        :param in_cert: str - Сертификат, для которого необходимо получить информацию.
+        :return: bytes - Информация о сертификате.
+        """
+        with self._lock:
+            return self._kc.x509_certificate_get_info(in_cert)
 
     def sign_data(self, data) -> bytes:
-        return self._kc.sign_data(data)
+        """
+        Подписывает данные.
+        :param data - Данные для подписи.
+        :return: bytes - Подписанные данные.
+        """
+        with self._lock:
+            return self._kc.sign_data(data)
 
     def verify_data(self, signature: str, data: str) -> dict[str, bytes]:
-        return self._kc.verify_data(signature, data)
+        """
+        Обеспечивает проверку подписи.
+        :param signature: str - Проверяемая подпись.
+        :param data: str - Проверяемые данные.
+        :return: dict[str, bytes] - Словарь, содержащий результат проверки.
+        """
+        with self._lock:
+            return self._kc.verify_data(signature, data)
 
     def x509_validate_certificate_ocsp(self, in_cert: str) -> dict[str, bytes]:
-        return self._kc.x509_validate_certificate_ocsp(in_cert)
+        """
+        Проверка заданного сертификата на валидность с помощью OCSP.
+        :param in_cert: str - Сертификат для проверки.
+        :return: dict[str, bytes] - Словарь, содержащий результат проверки.
+        """
+        with self._lock:
+            return self._kc.x509_validate_certificate_ocsp(in_cert)
 
     def x509_validate_certificate_crl(self, in_cert: str, crl_path: str) -> dict[str, bytes]:
-        return self._kc.x509_validate_certificate_crl(in_cert, crl_path)
+        """
+        Проверка заданного сертификата на валидность с помощью CRL.
+        :param in_cert: str - Сертификат для проверки.
+        :param crl_path: str - Путь к файлу CRL.
+        :return: dict[str, bytes] - Словарь, содержащий результат проверки.
+        """
+        with self._lock:
+            return self._kc.x509_validate_certificate_crl(in_cert, crl_path)
 
     def get_time_from_sign(self, sign: str) -> int:
-        return self._kc.get_time(sign)
+        """
+        Извлекает временную метку из подписи.
+        :param sign: str - подпись, из которой нужно извлечь временную метку.
+        :return: int - временная метка.
+        """
+        with self._lock:
+            return self._kc.get_time(sign)
 
     def set_tsa_url(self):
-        self._kc.set_tsa_url()
+        """
+        Установка адреса сервиса TSA.
+        """
+        with self._lock:
+            self._kc.set_tsa_url()

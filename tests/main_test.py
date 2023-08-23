1d4cc5b83385f17005df492ddaf1271c4574e587
Shalqarov <shalqarov01@gmail.com>
2023-08-23 15:16:03 +0600

    Refactor main_test.py for better modularity
    The previous structure of main_test.py was a bit haphazard with multiple levels of nested logic. It has been restructured to improve code modularity and readability. Code was split into smaller test functions, and constants were defined at the module level. A pytest fixture was also added to provide a common setup for all smaller test functions. Exception handling was adjusted to invoke pytest.fail directly.

--- tests/main_test.py
+++ tests/main_test.py
@@ -5,42 +5,13 @@ import pytest
 from dotenv import load_dotenv
 
 from pykalkan import Adapter
-from pykalkan.exceptions import KalkanException, ValidateException, ErrorCode
 
 load_dotenv()
 
-library = "libkalkancryptwr-64.so"
-data_to_sign = "SGVsbG8sIFdvcmxkIQ=="
-
-
-@pytest.mark.parametrize(
-    "path,password",
-    [
-        (os.getenv("CERT_PATH"), os.getenv("CERT_PASSWORD")),
-    ],
-)
-def test_main(path, password):
-    try:
-        kc = Adapter(library)
-        kc.init()
-        kc.set_tsa_url()
-        kc.load_key_store(path, password)
-        data = kc.sign_data(data_to_sign)
-        if not is_valid_date(kc.get_time_from_sign(data.decode())):
-            raise KalkanException(ErrorCode.INVALID_TIME, "GET TIME")
-        res = kc.verify_data(
-            data.decode(),
-            data_to_sign,
-        )
-        cert = res["Cert"].decode()
-        kc.x509_validate_certificate_ocsp(cert)
-        kc.x509_validate_certificate_crl(cert, os.getenv("CRL_PATH"))
-    except ValidateException as ve:
-        assert False, f"Validate failed: {ve}"
-    except KalkanException as ke:
-        assert False, f"Kalkan Exception failed: {ke}"
-    else:
-        kc.finalize()
+LIBRARY = "libkalkancryptwr-64.so"
+DATA_TO_SIGN = "SGVsbG8sIFdvcmxkIQ=="
+CERT_PATH = os.getenv("CERT_PATH")
+CERT_PASSWORD = os.getenv("CERT_PASSWORD")
 
 
 def is_valid_date(timestamp):
@@ -50,3 +21,54 @@ def is_valid_date(timestamp):
         return True
     except ValueError:
         return False
+
+
+@pytest.fixture(scope="module")
+def adapter():
+    try:
+        kc = Adapter(LIBRARY)
+        kc.init()
+        kc.load_key_store(CERT_PATH, CERT_PASSWORD)
+        kc.set_tsa_url()
+        yield kc
+        kc.finalize()
+    except OSError as e:
+        assert False, f"Adapter creation fail: {e}"
+    except Exception as e:
+        pytest.fail(e)
+
+
+def test_sign_data(adapter):
+    try:
+        data = adapter.sign_data(DATA_TO_SIGN)
+        assert data is not None, "Data signing failed"
+        timestamp = adapter.get_time_from_sign(data.decode())
+        assert is_valid_date(timestamp), "Invalid time in signed data"
+    except Exception as e:
+        pytest.fail(str(e))
+
+
+def test_verify_data(adapter):
+    try:
+        data = adapter.sign_data(DATA_TO_SIGN)
+        adapter.verify_data(data.decode(), DATA_TO_SIGN)
+    except Exception as e:
+        pytest.fail(str(e))
+
+
+def test_validate_cert_ocsp(adapter):
+    try:
+        data = adapter.sign_data(DATA_TO_SIGN)
+        res = adapter.verify_data(data.decode(), DATA_TO_SIGN)
+        adapter.x509_validate_certificate_ocsp(res["Cert"].decode())
+    except Exception as e:
+        pytest.fail(str(e))
+
+
+def test_validate_cert_crl(adapter):
+    try:
+        data = adapter.sign_data(DATA_TO_SIGN)
+        res = adapter.verify_data(data.decode(), DATA_TO_SIGN)
+        adapter.x509_validate_certificate_crl(res["Cert"].decode(), os.getenv("CRL_PATH"))
+    except Exception as e:
+        pytest.fail(str(e))
